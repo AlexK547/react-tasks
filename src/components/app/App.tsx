ec416112bb63f17ad0cff92ddc0824c9b87a1baf
Alex Kozhenov <kozhenov1984@mail.ru>
2024-07-06 14:33:45 +0400

    feat: create structure project

---
+++ src/components/app/App.tsx
@@ -0,0 +1,14 @@
+import Result from "../Result";
+import Search from "../Search";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="wrapper">
+      <Search />
+      <Result />
+    </div>
+  );
+}
+
+export default App;
